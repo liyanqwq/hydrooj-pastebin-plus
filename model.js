6dd02914411864ab89c39e04964dd4948dbcf083
立言 <liyanqwq@duianit.cn>
2022-02-09 14:53:58 +0800

    1.0.2: Fix bugs

--- model.js
+++ model.js
@@ -5,12 +5,26 @@ async function add(userId, username, title, content, isprivate) {
     const pasteId = String.random(32); // Hydro提供了此方法，创建一个长度为32的随机字符串
     // 使用 mongodb 为数据库驱动，相关操作参照其文档
     var d = new Date(),	str = '';
-	str += d.getFullYear() + '/'; //获取当前年份 
-	str += d.getMonth() + 1 + '/'; //获取当前月份（0——11） 
-	str += d.getDate() + ' ';
-	str += d.getHours() + ':';
-	str += d.getMinutes() + ':';
-	str += d.getSeconds();
+    function dateFormat(fmt, date) {
+        let ret;
+        const opt = {
+            "Y+": date.getFullYear().toString(),        // 年
+            "m+": (date.getMonth() + 1).toString(),     // 月
+            "d+": date.getDate().toString(),            // 日
+            "H+": date.getHours().toString(),           // 时
+            "M+": date.getMinutes().toString(),         // 分
+            "S+": date.getSeconds().toString()          // 秒
+            // 有其他格式化字符需求可以继续添加，必须转化成字符串
+        };
+        for (let k in opt) {
+            ret = new RegExp("(" + k + ")").exec(fmt);
+            if (ret) {
+                fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+            };
+        };
+        return fmt;
+    }
+    str = dateFormat("YYYY-mm-dd HH:MM:SS", d);
     const result = await coll.insertOne({
         _id: pasteId,
         title,
@@ -43,7 +57,7 @@ async function get(pasteId) {
 }
 
 async function getUserPaste(userId) {
-    return await coll.find({ "owner": Number.parseInt(userId) }).toArray();
+    return await coll.find({ "owner": Number.parseInt(userId) }).sort({ time: -1 }).toArray();
 }
 
 
